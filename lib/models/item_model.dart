a0e7406cd13001d82f9464f7d15bc135b25ff50f
Maxamed459 <maxamedmahdi459@gmail.com>
2026-04-04 11:31:09 +0300

    added factory for the ItemModel

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -7,4 +7,8 @@ class ItemModel {
   final double price;
 
   ItemModel({required this.id, required this.name, required this.imagePath, required this.description, required this.category, required this.price});
+  
+  factory ItemModel.fromJson(Map<String, dynamic> json) {
+    return ItemModel(id: json['product_id'], name: json['name'], imagePath: json['image_url'], description: json['description'], category: json['categories']?['name'], price: json['price']);
+  }
 }
