3ac927e44866b1ccacde2f8076ea3199d458c6fd
Maxamed459 <maxamedmahdi459@gmail.com>
2026-04-04 11:29:00 +0300

    added fetchProducts fun that fetches products from supabase and updated the fetchCategories

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -4,39 +4,58 @@ import 'package:pharmacy_app/models/item_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class HomeController extends GetxController {
-
   final RxInt currentIndex = 0.obs;
   final supabase = Supabase.instance.client;
   final _isloading = false.obs;
-  get isloading => _isloading.value;
+  bool get isloading => _isloading.value;
+  final _isCategoriesLoading = false.obs;
+  final _isProductsLoading = false.obs;
+  bool get isLoading => _isCategoriesLoading.value || _isProductsLoading.value;
 
   RxList<CategoryModel> categories = <CategoryModel>[].obs;
 
-  final RxList<ItemModel> bestSellerProduct = <ItemModel>[
-    ItemModel(id: '1', name: 'Liver cleans Detox', imagePath: 'assets/images/pill.png', description: 'Health Voda Liver cleans Detox & repair natural', category: 'Pills', price: 29.99),
-    ItemModel(id: '2', name: 'Sun Cream', imagePath: 'assets/images/creams.png', description: 'High quality Sun Cream natural 100%', category: 'Creams', price: 19.55),
-    ItemModel(id: '3', name: 'Vitamin capsules', imagePath: 'assets/images/vitamins.png', description: 'Health Vitamins for elder and middle age people for boosting immune system', category: 'Vitamins', price: 39.00),
-    ItemModel(id: '5', name: 'High Protein pills', imagePath: 'assets/images/supplements.png', description: 'High Protein supplements boosts energy and grows muscle', category: 'Supplements', price: 99.99),
-  ].obs;
+  RxList<ItemModel> bestSellerProduct = <ItemModel>[].obs;
 
   void updatedSelectedindex(int index) {
     currentIndex.value = index;
   }
 
   Future<List<CategoryModel>> fetchCategories() async {
-
     try {
-      _isloading.value = true;
-      final response = await supabase.from('categories').select('category_id, name, image_path');
+      _isCategoriesLoading.value = true;
+      final response = await supabase
+          .from('categories')
+          .select('category_id, name, image_path');
 
       final data = response.map((e) => CategoryModel.fromJson(e)).toList();
       // print('This is data $data');
       categories.value = data;
-      return categories;
 
+      return categories;
     } catch (e) {
       print('Error at this place: $e');
       return [];
+    } finally {
+      _isCategoriesLoading.value = false;
+    }
+  }
+
+  Future<List<ItemModel>> fetchProducts() async {
+    try {
+      _isProductsLoading.value = true;
+      final response = await supabase
+          .from('products')
+          .select(
+            'product_id, name, image_url, description, price, category_id, categories(name) ',
+          );
+      final data = response.map((e) => ItemModel.fromJson(e)).toList();
+      bestSellerProduct.value = data;
+      return bestSellerProduct;
+    } catch (e) {
+      print('Error at this place: $e');
+      return [];
+    } finally {
+      _isProductsLoading.value = false;
     }
   }
 
@@ -44,7 +63,6 @@ class HomeController extends GetxController {
   void onInit() {
     super.onInit();
     fetchCategories(); // Fetch categories when controller is initialized
+    fetchProducts();
   }
-
 }
-
